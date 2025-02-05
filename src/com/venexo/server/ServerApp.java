649ed70aee75bd38367bfd3d18d60b0ca66d2844
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2025-02-05 19:50:13 +0100

    feat(ServerApp): Added server logic to wait for at least 2 players

--- src/com/venexo/server/ServerApp.java
+++ src/com/venexo/server/ServerApp.java
@@ -1,5 +1,40 @@
 package com.venexo.server;
 
-public class ServerApp {
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
+import com.venexo.players.Player;
+import com.venexo.utils.Constants;
+
+public class ServerApp {
+  public static void main(String[] args) throws IOException {
+    boolean gameStarted = false;
+    ServerSocket serverSocket = new ServerSocket(Constants.SERVER_PORT);
+
+    ArrayList<Player> players = new ArrayList<>();
+
+    Socket clientSocket = serverSocket.accept();
+    DataInputStream playerInput = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+    DataOutputStream playerOutput = new DataOutputStream(new BufferedOutputStream(clientSocket.getOutputStream()));
+
+    String playerName = playerInput.readUTF();
+    System.out.println("Player " + playerName + " connected");
+
+    Player player = new Player(playerName, playerOutput, true);
+    players.add(player);
+
+    System.out.println(players.size());
+
+    if (players.size() >= 2 && players.size() < 3 && !gameStarted) {
+      gameStarted = true;
+    } else {
+      System.out.println("Waiting for 2 players");
+    }
+  }
 }
