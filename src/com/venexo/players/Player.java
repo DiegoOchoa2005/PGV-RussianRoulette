2fc3d062d757a8cd356846a1ad16c87b1fd6a05d
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2025-02-06 02:14:28 +0100

    refact(Player):Updated logic when a player is dead

--- src/com/venexo/players/Player.java
+++ src/com/venexo/players/Player.java
@@ -53,6 +53,12 @@ public class Player {
   public void getShot() {
     if (this.lives < 0) {
       this.lives = 0;
+
+      return;
+    }
+
+    if (this.lives == 0) {
+      this.isAlive = false;
       return;
     }
 
