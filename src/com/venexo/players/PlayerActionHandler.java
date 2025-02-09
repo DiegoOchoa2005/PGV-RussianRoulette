be4f6583aad3a86361ca55d381995906de732487
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2025-02-09 15:59:20 +0100

    feat(PlayerActionHandler): Implemented colors for players action

--- src/com/venexo/players/PlayerActionHandler.java
+++ src/com/venexo/players/PlayerActionHandler.java
@@ -2,6 +2,7 @@ package com.venexo.players;
 
 import java.io.IOException;
 
+import com.venexo.colors.ConsoleColors;
 import com.venexo.guns.Shotgun;
 import com.venexo.utils.RoundManager;
 
@@ -37,7 +38,7 @@ public class PlayerActionHandler {
       } else {
         try {
           this.roundManager.getPlayers().get(this.roundManager.getCurrentPlayer()).getMessage()
-              .writeUTF("Please choose a valid action!");
+              .writeUTF(ConsoleColors.changeColor("Please choose a valid action!", ConsoleColors.ANSI_RED));
           this.roundManager.getPlayers().get(this.roundManager.getCurrentPlayer()).getMessage().flush();
         } catch (IOException e) {
           e.printStackTrace();
@@ -56,12 +57,16 @@ public class PlayerActionHandler {
     switch (action) {
       case "player":
         this.roundManager
-            .setPlayerShooted(this.roundManager.getPlayers().get(this.roundManager.calcualteNextPlayer()).getName());
+            .setPlayerShooted(ConsoleColors.changeBoldColor(
+                this.roundManager.getPlayers().get(this.roundManager.calcualteNextPlayer()).getName(),
+                ConsoleColors.ANSI_YELLOW));
         this.shootAction(shootgun, action);
         break;
       case "myself":
         this.roundManager
-            .setPlayerShooted(this.roundManager.getPlayers().get(this.roundManager.getCurrentPlayer()).getName());
+            .setPlayerShooted(ConsoleColors.changeBoldColor(
+                this.roundManager.getPlayers().get(this.roundManager.getCurrentPlayer()).getName(),
+                ConsoleColors.ANSI_YELLOW));
         this.shootAction(shootgun, action);
         break;
     }
@@ -72,11 +77,20 @@ public class PlayerActionHandler {
     }
   }
 
-  private void announceShoot(String playerName, String bullet) {
+  private void announceShoot(String shooterName, String bullet, String actionDisplayed) {
+    String bulletName = bullet.equals("FAKE")
+        ? ConsoleColors.changeBoldColor(bullet + " BULLET TO ", ConsoleColors.ANSI_BLUE)
+        : ConsoleColors.changeBoldColor(bullet + " BULLET TO ", ConsoleColors.ANSI_RED);
+    String shotHimself = actionDisplayed.equalsIgnoreCase("myself")
+        ? ConsoleColors.changeBoldColor("HIMSELF", ConsoleColors.ANSI_YELLOW)
+        : ConsoleColors.changeBoldColor(this.roundManager.getPlayerShooted(), ConsoleColors.ANSI_YELLOW);
+
     for (Player player : this.roundManager.getPlayers()) {
       try {
         player.getMessage()
-            .writeUTF(playerName + " SHOOTS A " + bullet + " BULLET TO " + this.roundManager.getPlayerShooted());
+            .writeUTF(shooterName
+                + ConsoleColors.changeBoldColor(" SHOOTS A " + bulletName, ConsoleColors.ANSI_ORANGE)
+                + shotHimself);
         player.getMessage().flush();
       } catch (IOException e) {
         e.printStackTrace();
@@ -87,12 +101,13 @@ public class PlayerActionHandler {
   private void shootAction(Shotgun shootgun, String actionDisplayed) {
     int playerAffected = actionDisplayed.equalsIgnoreCase("myself") ? this.roundManager.getCurrentPlayer()
         : this.roundManager.calcualteNextPlayer();
-    String shooterName = this.roundManager.getPlayers().get(this.roundManager.getCurrentPlayer()).getName();
-    System.out.println(playerAffected);
+    String shooterName = ConsoleColors.changeBoldColor(
+        this.roundManager.getPlayers().get(this.roundManager.getCurrentPlayer()).getName(), ConsoleColors.ANSI_YELLOW);
+
     for (String bullet : shootgun.getShootgunBullets()) {
       if (!bullet.equals("EMPTY")) {
         if (bullet.equals("FAKE")) {
-          this.announceShoot(shooterName, bullet);
+          this.announceShoot(shooterName, bullet, actionDisplayed);
           shootgun.getShootgunBullets().set(shootgun.getShootgunBullets().indexOf(bullet), "EMPTY");
           shootgun.calculateCurrentFakeBullets();
 
@@ -103,7 +118,7 @@ public class PlayerActionHandler {
         }
 
         if (bullet.equals("REAL")) {
-          this.announceShoot(shooterName, bullet);
+          this.announceShoot(shooterName, bullet, actionDisplayed);
           this.roundManager.getPlayers().get(playerAffected).getShot();
           shootgun.getShootgunBullets().set(shootgun.getShootgunBullets().indexOf(bullet), "EMPTY");
           shootgun.calculateCurrentRealBullets();
